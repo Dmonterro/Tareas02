26c6e71df29dae12e0bb32ff57dab8b0123def5d
Diego Monterroso <149099355+Dmonterro@users.noreply.github.com>
2024-01-25 12:22:51 -0600

    Add files via upload

---
+++ NumHoras.py
@@ -0,0 +1,8 @@
+HorT = float(input("Ingrese el número de horas trabajadas: "))
+VaHor = float(input("Ingrese el costo por hora: "))
+
+
+Total= HorT * VaHor
+
+
+print("La paga correspondiente es: Q." + str(Total))
---
+++ PregunteNom.py
@@ -0,0 +1,4 @@
+nomUsuario = input("Por favor, introduce tu nombre: ")
+
+# Mostrar la cadena con el nombre del usuario
+print("Hola, " + nomUsuario + "! Bienvenido.")
